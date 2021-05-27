e9796a475377878ba999601e3f3b5def64727bf3
Bergony <Bergony@hotmail.com>
2021-05-27 19:54:14 -0300

    Add resonseFields to RestDocs to teste . close #5

--- src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -26,6 +26,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.description;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -54,11 +56,22 @@ class BeerControllerTest {
                 .andExpect(status().isOk())
                 .andDo(document("v1/beer",
                         pathParameters(
-                            parameterWithName("beerId").description("UUID of Desire beer to get")
-                            ),
+                                parameterWithName("beerId").description("UUID of Desire beer to get")
+                        ),
                         requestParameters(
                                 parameterWithName("iscold").description("Is beer cold Query param")
-                        )));
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Id of Beer"),
+                                fieldWithPath("version").description("Version number"),
+                                fieldWithPath("createdDate").description("Date Created"),
+                                fieldWithPath("lastModifiedDate").description("Date Updated"),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("UPC of Beer"),
+                                fieldWithPath("price").description("Price"),
+                                fieldWithPath("quantityOnHand").description("Quantity On hand")                        )
+                        ));
     }
 
     @Test
